92a928ebe45e2a2e541e695c56c3b65a1db4c4d7
rk18venom <rk.20u10641@btech.nitdgp.ac.in>
2022-07-12 00:35:09 +0530

    Day - 6

--- Arrays/Merge Sorted Arrays.py
+++ Arrays/Merge Sorted Arrays.py
@@ -8,3 +8,25 @@ def merge(nums1, m, nums2, n):
         arr.sort()
         for i in range(n+m):
             nums1[i]=arr[i]
+
+#Inplace
+
+def merge(A,B):
+    
+    m=len(A)
+    n=len(B)
+    i=0
+    j=0
+    k=0
+    
+    
+    while i<m:
+        if A[i]>B[j]:
+            A[i],B[j]=B[j],A[i]
+        i+=1
+    else:
+        while j<n:
+            A.append(B[j])
+            j+=1
+        A.sort()
+            
