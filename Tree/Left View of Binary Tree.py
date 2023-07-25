eeaa4361fbfbd0d0fe77f74cd982a090643d06f9
rk18venom <rk.20u10641@btech.nitdgp.ac.in>
2023-07-26 02:11:15 +0530

    Left View and Right View of Binary Tree 26July2023

---
+++ Tree/Left View of Binary Tree.py
@@ -0,0 +1,43 @@
+from queue import Queue
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def takeinput():
+    arr=[int(x) for x in input().split()]
+    i=0
+    q=Queue()
+    root=Node(arr[i])
+    q.put(root)
+    while (i<len(arr)//2) and q.empty()!=True:
+        it=q.get()
+        if arr[2*i+1]!=-1:
+            it.left=Node(arr[2*i+1])
+            q.put(it.left)
+        else:
+            it.left=None
+        
+        if arr[2*i+2]!=-1:
+            it.right=Node(arr[2*i+2])
+            q.put(it.right)
+        else:
+            it.right=None
+        i+=1
+    return root
+
+def leftView(root, level, res):
+    if root is None:
+        return None
+    # If the current node's depth equals to `level`, append its value into result list.
+    if len(res)==level:
+        res.append(root.data)
+    leftView(root.left, level+1, res)
+    leftView(root.right, level+1, res)
+
+root=takeinput()
+res=[]
+leftView(root, 0, res)
+print("Left View of Binary Tree: {}".format(res))
