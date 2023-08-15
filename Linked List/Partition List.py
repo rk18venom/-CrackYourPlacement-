39d6909b990b543b8d38658d2c70d105173b5d30
rk18venom <rk.20u10641@btech.nitdgp.ac.in>
2023-08-15 17:15:46 +0530

    Leetcode 15 August 2023

---
+++ Linked List/Partition List.py
@@ -0,0 +1,29 @@
+#Problem Link: https://leetcode.com/problems/partition-list/description/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head, x):
+        if not head or not head.next:
+            return head
+        smaller = ListNode(-1)
+        greater = ListNode(-1)
+        curr1 = smaller
+        curr2 = greater
+        temp = head
+        
+        while temp:
+            if temp.val < x:
+                curr1.next = temp
+                curr1 = curr1.next
+            else:
+                curr2.next = temp
+                curr2 = curr2.next
+            temp = temp.next
+        
+        curr2.next = None
+        curr1.next = greater.next
+        return smaller.next
