3019e88f4c0e56befa5ed267619f26ec9bae2740
rk18venom <rk.20u10641@btech.nitdgp.ac.in>
2024-11-15 12:02:01 +0530

    Added more approach

--- Heap/Second Largest.py
+++ Heap/Second Largest.py
@@ -15,6 +15,32 @@ class Solution:
             return min(ans)
         else:
             return -1
+
+    def getSecondLargest2(self, arr):
+        # Code Here
+        prev=-1
+        maxi=-1
+        for x in arr:
+            if maxi<x:
+                prev=maxi
+                maxi=x
+            elif x!=maxi:
+                prev=max(x,prev)
+        return prev
+
+    def getSecondLargest3(self, arr):
+        first_largest, second_largest = -1, -1
+
+        for i in arr:
+            if i > first_largest:
+                second_largest = first_largest
+                first_largest = i
+            elif i > second_largest and i < first_largest:
+                second_largest = i
+    
+        return second_largest
+                
+
 #Main
 t=int(input())
 for _ in range(t):
