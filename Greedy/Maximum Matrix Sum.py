f6727a13e1cb1a1ecb8db1543732614c7fd7f845
rk18venom <rk.20u10641@btech.nitdgp.ac.in>
2024-11-24 22:46:33 +0530

    LEETCODE POTD 24 NOV 2024

---
+++ Greedy/Maximum Matrix Sum.py
@@ -0,0 +1,31 @@
+#Link: https://leetcode.com/problems/maximum-matrix-sum/description/
+
+#Solution
+
+class Solution:
+    def maxMatrixSum(self, matrix):
+        abs_min = float('inf') # minimum absolulte value in matrix
+        cnt = 0 # counter of negative number
+        abs_sum = 0 # sum of absolute values
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] < 0:
+                    cnt += 1
+                
+                abs_min = min(abs(matrix[i][j]), abs_min) #
+                abs_sum += abs(matrix[i][j])
+            
+        return abs_sum if cnt % 2 == 0 else abs_sum - abs_min * 2
+    
+#Main
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    matrix=[]
+    for i in range(n):
+        row=list(map(int,input().split()))
+        matrix.append(row)
+    s=Solution()
+    print(s.maxMatrixSum(matrix))
